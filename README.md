21cdadb66c906c19f1b21687512dcb081caed7c9
Roni Riska <roni.riska@nokia.com>
2019-09-03 11:58:07 +0300

    Add tracer configuration
    The tracer can be configured with environment variables.
    By default a no-op tracer is still create.
    
    Change-Id: I508b69fdc62ff46f3978c6204824c4900a5c6e3c
    Signed-off-by: Roni Riska <roni.riska@nokia.com>

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # RIC tracing helper library
 
-The library includes a function for creating a tracer instance.
-
-ToDo: tracer configuration options
+The library includes a function for creating a configured tracer instance.
+It hides the underlaying tracer implementation from the application.
 
 ## Usage
 
@@ -19,6 +18,22 @@ TextMap carrier and JSON format serialization.
 The [opentracing C++](https://github.com/opentracing/opentracing-cpp) Readme gives examples
 how span context **inject** and **extract** with textmap can be done.
 Serialization to JSON can be done with any JSON library.
+
+## Configuration
+
+The trace library currently supports only [Jaeger](https://www.jaegertracing.io/) [C++ client](https://github.com/jaegertracing/jaeger-client-cpp) tracer implementation.
+The configuration is done using environment variables:
+
+| environment variable         | values                              | default        |
+| ---------------------------- |------------------------------------ | -------------- |
+| TRACING_ENABLED              | 1, true, 0, false                   | false          |
+| TRACING_JAEGER_SAMPLER_TYPE  | const, propabilistic, ratelimiting  | const          |
+| TRACING_JAEGER_SAMPLER_PARAM | float                               | 0.001          |
+| TRACING_JAEGER_AGENT_ADDR    | IP addr + port                      | 127.0.0.1:6831 |
+| TRACING_JAEGER_LOG_LEVEL     | all, error, none                    | none           |
+
+Meaning of the configuration variables is described in Jaeger web pages.
+By default a no-op tracer is created.
 
 ## Requires
 
@@ -44,6 +59,21 @@ you are building in `build` dir under the tracelibcpp
 ```shell
 lcov -c --no-external --base-directory $(dirname $PWD)  --directory . --output-file cov.info
 genhtml cov.info
+```
+
+## Binary package support
+Binary packages of the libary can be created with `make package` target, or with
+the Dockerfile in the `ci` directory.
+
+The Docker build executes unit tests and compiles binary packages which can then be
+exported from the container by running it and giving the target directory as a command line
+argument. The target directory must mounted to the container.
+
+```shell
+# Build the container
+docker build -t tracelibcpp -f ci/Dockerfile .
+# Export binary packages to /tmp
+docker run -v /tmp:/tmp tracelibcpp /tmp
 ```
 
 ## License
--- src/tracelib.cpp
+++ src/tracelib.cpp
@@ -16,13 +16,100 @@
  */
 
 #include "tracelibcpp.hpp"
+#include "config.hpp"
 
-#include <jaegertracing/Tracer.h>
+using namespace tracelibcpp;
+
+std::string ConfMaker::getEnv(const char* envName, std::string defVal)
+{
+    const char *ev = getenv(envName);
+    if (!ev)
+        return defVal;
+    return std::string(ev);
+}
+
+bool ConfMaker::isTracingEnabled()
+{
+    std::string envValue = getEnv(TRACING_ENABLED_ENV, "false");
+    if (envValue == "1" || boost::iequals(envValue, "true"))
+        return true;
+    else
+        return false;
+}
+
+jaegertracing::Config ConfMaker::makeNopTraceConfig()
+{
+    return jaegertracing::Config(true,
+            jaegertracing::samplers::Config("const", 0));
+}
+
+jaegertracing::samplers::Config ConfMaker::getSamplerConfig()
+{
+    std::string samplerType = getEnv(JAEGER_SAMPLER_TYPE_ENV, "const");
+    // Use value 0.001 as default param, same way as jaeger does it
+    double param = atof(getEnv(JAEGER_SAMPLER_PARAM_ENV, "0.001").c_str());
+    return jaegertracing::samplers::Config(samplerType, param);
+}
+
+jaegertracing::reporters::Config ConfMaker::getReporterConfig()
+{
+    std::string agentHostPort = getEnv(JAEGER_AGENT_ADDR_ENV, jaegertracing::reporters::Config::kDefaultLocalAgentHostPort);
+
+    if (agentHostPort.find(':') == std::string::npos)
+        agentHostPort += ":6831";
+
+    return jaegertracing::reporters::Config(
+        0, std::chrono::seconds(0),     // use jaeger defaults
+        getLoggingLevel() == LOG_ALL,   // log spans
+        agentHostPort
+    );
+}
+
+LogLevel ConfMaker::getLoggingLevel()
+{
+    std::string logLevel = getEnv(JAEGER_LOG_LEVEL_ENV, "error");
+    if (boost::iequals(logLevel, "all"))
+        return LOG_ALL;
+    else if (boost::iequals(logLevel, "error"))
+        return LOG_ERR;
+    else
+        return LOG_NONE;
+}
+
+std::unique_ptr<jaegertracing::logging::Logger> ConfMaker::getLogger()
+{
+    switch (getLoggingLevel())
+    {
+        case LOG_ALL:
+        case LOG_ERR:
+            return jaegertracing::logging::consoleLogger();
+            break;
+        default:
+            return jaegertracing::logging::nullLogger();
+    }
+}
+
+jaegertracing::Config ConfMaker::getTraceConfig()
+{
+    if (!isTracingEnabled())
+        return makeNopTraceConfig();
+    auto sampler = getSamplerConfig();
+    auto reporter = getReporterConfig();
+    return jaegertracing::Config(false, sampler, reporter);
+}
 
 std::shared_ptr<opentracing::Tracer> tracelibcpp::createTracer(std::string serviceName)
 {
-    auto config = jaegertracing::Config(true,
-                jaegertracing::samplers::Config("const", 0));
-    return jaegertracing::Tracer::make(serviceName, config,  jaegertracing::logging::consoleLogger());
+    auto cm = ConfMaker(serviceName);
+    auto config = cm.getTraceConfig();
+    try {
+        return jaegertracing::Tracer::make(serviceName, config, cm.getLogger());
+    } catch (std::exception& e)
+    {
+        if (cm.getLoggingLevel() != LOG_NONE)
+            std::cerr << "Cannot create tracer: " << e.what() << std::endl;
+        return jaegertracing::Tracer::make(serviceName, cm.makeNopTraceConfig());
+    }
 }
+
 
